c60556bb0bbcb38a61917e52c631f14acbd73efe
Sakib <sakib.hstu.bd@gmail.com>
2023-05-18 17:09:39 +0600

    navbar added

---
+++ src/Pages/Shared/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from '../../assets/logo.jpg'
+import ActiveLink from '../../ActiveLink/ActiveLink';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar bg-secondary text-white p-3">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ActiveLink to='/'>Home</ActiveLink>
+                        <ActiveLink to='/allToys'>All Toys</ActiveLink>
+                        <ActiveLink to='/myToys'>My Toys</ActiveLink>
+                        <ActiveLink to='/addAToys'>Add A Toy</ActiveLink>
+                        <ActiveLink to='/blog'>Blog</ActiveLink>
+                    </ul>
+                </div>
+
+                <div className='flex items-center gap-1'>
+                    <img className='w-16 h-16 rounded-full' src={logo} alt="" />
+                    <h1 className="text-4xl font-bold">Car Zone <span className='text-primary'>Kids</span></h1>
+
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-xl font-bold gap-x-10">
+                    <ActiveLink to='/'>Home</ActiveLink>
+                    <ActiveLink to='/allToys'>All Toys</ActiveLink>
+                    <ActiveLink to='/myToys'>My Toys</ActiveLink>
+                    <ActiveLink to='/addAToys'>Add A Toy</ActiveLink>
+                    <ActiveLink to='/blog'>Blog</ActiveLink>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn bg-primary">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#FF6F96',
+        secondary:'#203D6A'
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
