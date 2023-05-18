b0aad20d72fd7d866a6f95c580857d0d21cff3f2
Sakib <sakib.hstu.bd@gmail.com>
2023-05-18 17:32:58 +0600

    footer added

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import logo from '../../assets/logo.jpg'
 
 const Footer = () => {
     return (
-        <div>
-            <h1>footer page</h1>
-        </div>
+        <footer className="footer p-10 bg-secondary text-white text-lg">
+            <div>
+                <img className='h-20 w-20 rounded-full' src={logo} alt="" />
+                <p>Car Zone Kids Ltm.<br />Providing reliable store since 1992</p>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </div>
+            <div>
+                <span className="footer-title">Store Information</span>
+                <p>Address: Uttara Sector 12 park - Dhaka</p>
+                <p>Email: carzonekids@shop.com</p>
+                <p>Tele: (+88) 9276-543-210</p>
+                
+            </div>
+        </footer>
     );
 };
 
--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
         <nav className="navbar bg-secondary text-white p-3">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <label tabIndex={0} className="btn bg-primary lg:hidden mr-5">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-secondary rounded-box w-52">
                         <ActiveLink to='/'>Home</ActiveLink>
                         <ActiveLink to='/allToys'>All Toys</ActiveLink>
                         <ActiveLink to='/myToys'>My Toys</ActiveLink>
@@ -21,7 +21,7 @@ const Navbar = () => {
 
                 <div className='flex items-center gap-1'>
                     <img className='w-16 h-16 rounded-full' src={logo} alt="" />
-                    <h1 className="text-4xl font-bold">Car Zone <span className='text-primary'>Kids</span></h1>
+                    <h1 className="text-lg md:text-4xl md:font-bold">Car Zone <span className='text-primary'>Kids</span></h1>
 
                 </div>
             </div>
