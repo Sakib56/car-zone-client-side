d30655e9ab5bea2fe2899ecf055facbe33efbae8
Sakib <sakib.hstu.bd@gmail.com>
2023-05-18 17:59:35 +0600

    banner section added

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import banner from '../../assets/banner.jpg'
 
 const Banner = () => {
     return (
         <div>
             <div>
-                
+                <img className='w-full h-[500px]' src={banner} alt="" />
             </div>
         </div>
     );
